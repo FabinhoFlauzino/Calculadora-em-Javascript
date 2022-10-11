b29eafd939e3cda62199dacc3f4bc464272fb4f8
Fabio Flauzino <fabio@mfrural.com.br>
2022-10-11 18:28:55 -0300

    subindo repositorio

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,77 +1,67 @@
 class CalcController {
+  constructor() {
+    this._locale = "pt-BR"
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+    this.initButtonsEvents();
+  }
 
-    constructor(){
-        this._locale = 'pt-BR';
-        this._displayCalcEl =  document.querySelector("#display");
-        this._dateEl = document.querySelector("#data");
-        this._timeEl = document.querySelector("#hora");
-        this._currentDate;
-        this.initialize();
-    }
+  initialize() {
+    this.setDisplayDateTime();
+    setInterval(() => {
+      this.setDisplayDateTime();
+    }, 1000)
+  }
 
-    initialize(){
+  initButtonsEvents(){
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        this.setDisplayDateTime();
+    buttons.forEach(btn => {
+      btn.addEventListener('click', (e, index) => {
+        console.log(btn.className.baseVal.replace("btn-", ""));
+      })
+    })
+  }
 
-        setInterval(()=>{
-            
-            this.setDisplayDateTime();
+  setDisplayDateTime(){
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit", month: "long", year: "numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
 
-        }, 1000);
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
 
-    }
+  set displayTime(value) {
+    this._timeEl.innerHTML = value;
+  }
 
-    setDisplayDateTime(){
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
 
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
-            day: "2-digit",
-            month: "long",
-            year: "numeric"
-        });
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-    }
+  set displayDate(value) {
+    this._dateEl.innerHTML = value;
+  }
 
-    get displayTime(){
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
 
-        return this._timeEl.innerHTML;
-    }
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
 
-    set displayTime(value){
+  get currentDate() {
+    return new Date();
+  }
 
-        return this._timeEl.innerHTML = value;
-    }
-
-    get displayDate(){
-
-        return this._dateEl.innerHTML;
-
-    }
-
-    set displayDate(value){
-
-        this._dateEl.innerHTML = value;
-
-    }
-
-    get displayCalc(){
-        return this._displayCalcEl.innerHTML;
-
-    }
-
-    set displayCalc(value){
-        this._displayCalcEl.innerHTML = value;
-
-    }
-
-    get currentDate(){
-        return new Date();
-
-    }
-
-    set currentDate(value){
-        this._currentDate = value;
-
-    }
-
-
+  set currentDate(value) {
+    this._currentDate = value;
+  }
 }
